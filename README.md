104909b1f17b5a0edae0f89b911e2bcf4755bbee
Alan Rodrigo <axtlan@Alans-MacBook-Air-2.local>
2021-11-15 19:57:36 -0600

    Chat works, it's done

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
+# Motivación
+
 El objetivo de este código es entender el funcionamiento de los websockets creando una aplicación simple de chat.
 
 - Uso de la libreria Tokio para las operaciones async 
 - Creación de un servidor para chat
     Basicamente es permitir la conección de multiples Tcp clients al servidor del chat y cuando un cliente envia un mensaje al server el mensaje será transmitido (broadcasted) a todos los demás clientes que estan conectados al servidor.
 
-Proyecto movido a /Rust-codes/
+
+# Compilación
+
+1. Clonar el repositorio.
+
+2. Ejecutar.
+
+```bash
+cargo run
+```
+
+3. En otras terminales usar telnet como clinte.
+
+```bash
+telnet localhost 8000
+```
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use tokio::net::{TcpListener};
 use tokio::io::{BufReader, AsyncBufReadExt, AsyncWriteExt};
+use tokio::sync::broadcast;
 
 
 // Funcion que me devuelte el valor por default de T
-fn give_me_default<T>() -> T  where T: Default {
-    Default::default()
-}
+// fn give_me_default<T>() -> T  where T: Default {
+//     Default::default()
+// }
+// Turbo fish operator
+// let value = give_me_default::<i32>();
 
 
 // Por default Rust no entiende el async en la funcion main, necesitamos la libreria Tokio
@@ -17,22 +20,49 @@ async fn main() {
     // Observando el tipo de retorno, impl Future<Output = Result<TcpListener, Error>>
     let listener = TcpListener::bind("localhost:8000").await.unwrap();
 
-    let (mut socket, _addr) = listener.accept().await.unwrap();
+    // Crearemos un canal que nos permitira hacer que multiples clientes puedan leer a los demas.
+    // Ya no va ser un string pues hemos anadido que sea una tupla
+    let (tx, _rx) = broadcast::channel(10 /*la cantidad de conecciones permitidas */);
 
-    let (reader, mut writer) = socket.split();
+        
+    loop {
+        let (mut socket, addr) = listener.accept().await.unwrap();
 
-    let mut reader = BufReader::new(reader);
-    let mut line = String::new();
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
 
+        
+        // Para manejar una tarea que puede ser manejada por multiples clientes
+        tokio::spawn( async move {
+            let (reader, mut writer) = socket.split();
 
-    loop {  
-        // Todo lo que llegue al socket lo leemos y lo guardamos en el buffer
-        let bytes_read  = reader.read_line(&mut line).await.unwrap(); // Regresa el numero de bytes 
-        if bytes_read == 0 {
-            break;
-        }
-        // Haremos uso de AsyncWriteExt para escribir en el buffer
-        writer.write_all(line.as_bytes()).await.unwrap()
-        line.clear();
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+
+            loop {  
+                // Agregamos el macro que nos permitira correr multiples cosas async al mismo tiempo
+                tokio::select! {
+                    
+                    // Todo lo que llegue al socket lo leemos y lo guardamos en el buffer
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+                        // Para poder recibir mensajes en el canal
+                        // Agregamos la direccion de donde llego para mandarla al mismo sitio
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        let (msg, other_addr) = result.unwrap();
+
+                        if addr != other_addr {
+                            // Haremos uso de AsyncWriteExt para escribir en el buffer
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+                        }
+                    }
+                }
+            }
+        });
     }
 }
